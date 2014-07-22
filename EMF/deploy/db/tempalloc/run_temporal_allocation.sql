2920849bb928ffd2a2970e391dad0df8a9b5bb33
Catherine Seppanen <cas@whpress.com>
2014-07-22 14:10:45 -0400

    use monthly inventory values if available

--- EMF/deploy/db/tempalloc/run_temporal_allocation.sql
+++ EMF/deploy/db/tempalloc/run_temporal_allocation.sql
@@ -37,10 +37,14 @@ DECLARE
   inv_processid varchar(64) := 'inv.segment';
   inv_emissions varchar(64) := 'inv.ann_emis';
   
+  orl_inv_month smallint := 0;
+  flat_file_monthly boolean := false;
+  
   xref_matching_sql text;
   
   month_num_sql text := '';
-  month_name_sql text := '';
+  prof_month_name_sql text := '';
+  inv_month_name_sql text := '';
   loop_date date;
   is_weekend boolean;
   add_day boolean;
@@ -48,30 +52,52 @@ DECLARE
   day_name_sql text := '';
 BEGIN
 
-	-- get the inventory table name
-	SELECT LOWER(i.table_name)
-	  INTO inventory_table_name
-	  FROM emf.internal_sources i
-	 WHERE i.dataset_id = input_dataset_id;
+  -- get the inventory table name
+  SELECT LOWER(i.table_name)
+    INTO inventory_table_name
+    FROM emf.internal_sources i
+   WHERE i.dataset_id = input_dataset_id;
 
-	-- get the inventory dataset type
-	SELECT dataset_types.name
-	  INTO inventory_dataset_type_name
-	  FROM emf.datasets
-	  JOIN emf.dataset_types
-	    ON dataset_types.id = datasets.dataset_type
-	 WHERE datasets.id = input_dataset_id;
+  -- get the inventory dataset type
+  SELECT dataset_types.name
+    INTO inventory_dataset_type_name
+    FROM emf.datasets
+    JOIN emf.dataset_types
+      ON dataset_types.id = datasets.dataset_type
+   WHERE datasets.id = input_dataset_id;
 
-  -- set data field names
   IF inventory_dataset_type_name = 'Flat File 2010 Point' OR
      inventory_dataset_type_name = 'Flat File 2010 Nonpoint' THEN
+
+    -- set data field names
     is_flat_file_inventory := true;
-		inv_fips := 'inv.region_cd';
-		inv_plantid := 'inv.facility_id';
-		inv_pointid := 'inv.unit_id';
-		inv_stackid := 'inv.rel_point_id';
-		inv_processid := 'inv.process_id';
-		inv_emissions := 'inv.ann_value';
+    inv_fips := 'inv.region_cd';
+    inv_plantid := 'inv.facility_id';
+    inv_pointid := 'inv.unit_id';
+    inv_stackid := 'inv.rel_point_id';
+    inv_processid := 'inv.process_id';
+    inv_emissions := 'inv.ann_value';
+    
+    -- check if inventory has monthly values
+    EXECUTE '
+    SELECT COUNT(*) > 0
+      FROM emissions.' || inventory_table_name || '
+     WHERE COALESCE(jan_value, feb_value, mar_value, apr_value, 
+                    may_value, jun_value, jul_value, aug_value, 
+                    sep_value, oct_value, nov_value, dec_value) IS NOT NULL'
+      INTO flat_file_monthly;
+  ELSE
+
+    -- check if ORL inventory is monthly (returns 0 if annual)
+    SELECT public.get_dataset_month(input_dataset_id)
+      INTO orl_inv_month;
+  
+    -- if monthly inventory, confirm that it falls in requested time period
+    IF orl_inv_month != 0 AND
+       (orl_inv_month < EXTRACT(MONTH FROM start_day) OR
+        orl_inv_month > EXTRACT(MONTH FROM end_day)) THEN
+      RETURN;
+    END IF;
   END IF;
   
   -- get the cross-reference and profile dataset ids and versions
@@ -140,83 +166,181 @@ BEGIN
   FOR month_num IN EXTRACT(MONTH FROM start_day)..EXTRACT(MONTH FROM end_day) LOOP
     IF LENGTH(month_num_sql) > 0 THEN
       month_num_sql := month_num_sql || ',';
-      month_name_sql := month_name_sql || ',';
+      prof_month_name_sql := prof_month_name_sql || ',';
+      inv_month_name_sql := inv_month_name_sql || ',';
     END IF;
     month_num_sql := month_num_sql || month_num;
     CASE month_num
       WHEN 1 THEN
-        month_name_sql := month_name_sql || 'january';
+        prof_month_name_sql := prof_month_name_sql || 'january';
+        inv_month_name_sql := inv_month_name_sql || 'jan_value';
       WHEN 2 THEN
-        month_name_sql := month_name_sql || 'february';
+        prof_month_name_sql := prof_month_name_sql || 'february';
+        inv_month_name_sql := inv_month_name_sql || 'feb_value';
       WHEN 3 THEN
-        month_name_sql := month_name_sql || 'march';
+        prof_month_name_sql := prof_month_name_sql || 'march';
+        inv_month_name_sql := inv_month_name_sql || 'mar_value';
       WHEN 4 THEN
-        month_name_sql := month_name_sql || 'april';
+        prof_month_name_sql := prof_month_name_sql || 'april';
+        inv_month_name_sql := inv_month_name_sql || 'apr_value';
       WHEN 5 THEN
-        month_name_sql := month_name_sql || 'may';
+        prof_month_name_sql := prof_month_name_sql || 'may';
+        inv_month_name_sql := inv_month_name_sql || 'may_value';
       WHEN 6 THEN
-        month_name_sql := month_name_sql || 'june';
+        prof_month_name_sql := prof_month_name_sql || 'june';
+        inv_month_name_sql := inv_month_name_sql || 'jun_value';
       WHEN 7 THEN
-        month_name_sql := month_name_sql || 'july';
+        prof_month_name_sql := prof_month_name_sql || 'july';
+        inv_month_name_sql := inv_month_name_sql || 'jul_value';
       WHEN 8 THEN
-        month_name_sql := month_name_sql || 'august';
+        prof_month_name_sql := prof_month_name_sql || 'august';
+        inv_month_name_sql := inv_month_name_sql || 'aug_value';
       WHEN 9 THEN
-        month_name_sql := month_name_sql || 'september';
+        prof_month_name_sql := prof_month_name_sql || 'september';
+        inv_month_name_sql := inv_month_name_sql || 'sep_value';
       WHEN 10 THEN
-        month_name_sql := month_name_sql || 'october';
+        prof_month_name_sql := prof_month_name_sql || 'october';
+        inv_month_name_sql := inv_month_name_sql || 'oct_value';
       WHEN 11 THEN
-        month_name_sql := month_name_sql || 'november';
+        prof_month_name_sql := prof_month_name_sql || 'november';
+        inv_month_name_sql := inv_month_name_sql || 'nov_value';
       WHEN 12 THEN
-        month_name_sql := month_name_sql || 'december';
+        prof_month_name_sql := prof_month_name_sql || 'december';
+        inv_month_name_sql := inv_month_name_sql || 'dec_value';
     END CASE;
   END LOOP;
 
-  -- calculate monthly totals from annual emissions
-  EXECUTE '
-  INSERT INTO emissions.' || monthly_result_table_name || ' (
-         dataset_id,
-         poll,
-         scc,
-         fips,
-         plantid,
-         pointid,
-         stackid,
-         processid,
-         profile_id,
-         fraction,
-         month,
-         total_emis,
-         days_in_month,
-         inv_dataset_id,
-         inv_record_id
-  )
-  SELECT ' || monthly_result_dataset_id || ',
-         inv.poll,
-         inv.scc,
-         ' || inv_fips || ',
-         ' || inv_plantid || ',
-         ' || inv_pointid || ',
-         ' || inv_stackid || ',
-         ' || inv_processid || ',
-         prof.profile_id,
-         unnest(array[' || month_name_sql || ']),
-         unnest(array[' || month_num_sql || ']),
-         ' || inv_emissions || ' * unnest(array[' || month_name_sql || ']),
-         public.get_days_in_month(unnest(array[' || month_num_sql || '])::smallint, ' || inventory_year || '::smallint),
-         inv.dataset_id,
-         inv.record_id
-    FROM emissions.' || inventory_table_name || ' inv
-    JOIN (' || xref_matching_sql || ') xref
-      ON xref.record_id = inv.record_id
-    JOIN emissions.' || monthly_profile_table_name || ' prof
-      ON prof.profile_id = xref.profile_id
-   WHERE ' || public.build_version_where_filter(input_dataset_id, input_dataset_version, 'inv') || '
-     AND ' || public.build_version_where_filter(monthly_profile_dataset_id, monthly_profile_dataset_version, 'prof');
+  IF orl_inv_month = 0 AND NOT flat_file_monthly THEN
+    -- calculate monthly totals from annual emissions
+    EXECUTE '
+    INSERT INTO emissions.' || monthly_result_table_name || ' (
+           dataset_id,
+           poll,
+           scc,
+           fips,
+           plantid,
+           pointid,
+           stackid,
+           processid,
+           profile_id,
+           fraction,
+           month,
+           total_emis,
+           days_in_month,
+           inv_dataset_id,
+           inv_record_id
+    )
+    SELECT ' || monthly_result_dataset_id || ',
+           inv.poll,
+           inv.scc,
+           ' || inv_fips || ',
+           ' || inv_plantid || ',
+           ' || inv_pointid || ',
+           ' || inv_stackid || ',
+           ' || inv_processid || ',
+           prof.profile_id,
+           unnest(array[' || prof_month_name_sql || ']),
+           unnest(array[' || month_num_sql || ']),
+           ' || inv_emissions || ' * unnest(array[' || prof_month_name_sql || ']),
+           public.get_days_in_month(unnest(array[' || month_num_sql || '])::smallint, ' || inventory_year || '::smallint),
+           inv.dataset_id,
+           inv.record_id
+      FROM emissions.' || inventory_table_name || ' inv
+      JOIN (' || xref_matching_sql || ') xref
+        ON xref.record_id = inv.record_id
+      JOIN emissions.' || monthly_profile_table_name || ' prof
+        ON prof.profile_id = xref.profile_id
+     WHERE ' || public.build_version_where_filter(input_dataset_id, input_dataset_version, 'inv') || '
+       AND ' || public.build_version_where_filter(monthly_profile_dataset_id, monthly_profile_dataset_version, 'prof');
+  
+    -- calculate monthly average day emissions
+    EXECUTE '
+    UPDATE emissions.' || monthly_result_table_name || '
+       SET avg_day_emis = total_emis / days_in_month';
 
-  -- calculate monthly average day emissions
-  EXECUTE '
-  UPDATE emissions.' || monthly_result_table_name || '
-     SET avg_day_emis = total_emis / days_in_month';
+  ELSIF flat_file_monthly THEN
+    -- populate monthly results from FF10 inventory
+    EXECUTE '
+    INSERT INTO emissions.' || monthly_result_table_name || ' (
+           dataset_id,
+           poll,
+           scc,
+           fips,
+           plantid,
+           pointid,
+           stackid,
+           processid,
+           profile_id,
+           fraction,
+           month,
+           total_emis,
+           days_in_month,
+           inv_dataset_id,
+           inv_record_id
+    )
+    SELECT ' || monthly_result_dataset_id || ',
+           inv.poll,
+           inv.scc,
+           ' || inv_fips || ',
+           ' || inv_plantid || ',
+           ' || inv_pointid || ',
+           ' || inv_stackid || ',
+           ' || inv_processid || ',
+           NULL,
+           1,
+           unnest(array[' || month_num_sql || ']),
+           unnest(array[' || inv_month_name_sql || ']),
+           public.get_days_in_month(unnest(array[' || month_num_sql || '])::smallint, ' || inventory_year || '::smallint),
+           inv.dataset_id,
+           inv.record_id
+      FROM emissions.' || inventory_table_name || ' inv
+     WHERE ' || public.build_version_where_filter(input_dataset_id, input_dataset_version, 'inv');
+  
+    -- calculate monthly average day emissions
+    EXECUTE '
+    UPDATE emissions.' || monthly_result_table_name || '
+       SET avg_day_emis = total_emis / days_in_month';
+
+  ELSE
+    -- populate monthly results from ORL inventory
+    EXECUTE '
+    INSERT INTO emissions.' || monthly_result_table_name || ' (
+           dataset_id,
+           poll,
+           scc,
+           fips,
+           plantid,
+           pointid,
+           stackid,
+           processid,
+           profile_id,
+           fraction,
+           month,
+           total_emis,
+           days_in_month,
+           avg_day_emis,
+           inv_dataset_id,
+           inv_record_id
+    )
+    SELECT ' || monthly_result_dataset_id || ',
+           inv.poll,
+           inv.scc,
+           ' || inv_fips || ',
+           ' || inv_plantid || ',
+           ' || inv_pointid || ',
+           ' || inv_stackid || ',
+           ' || inv_processid || ',
+           NULL,
+           1,
+           ' || orl_inv_month || ',
+           inv.avd_emis * public.get_days_in_month(' || orl_inv_month || '::smallint, ' || inventory_year || '::smallint),
+           public.get_days_in_month(' || orl_inv_month || '::smallint, ' || inventory_year || '::smallint),
+           inv.avd_emis,
+           inv.dataset_id,
+           inv.record_id
+      FROM emissions.' || inventory_table_name || ' inv
+     WHERE ' || public.build_version_where_filter(input_dataset_id, input_dataset_version, 'inv');
+  END IF;
 
   IF resolution LIKE '%month%' THEN
     RETURN;
