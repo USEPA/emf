2efc1c89344f3fe048fedad3c3427f6f5fb1f326
Catherine Seppanen <cas@whpress.com>
2014-09-14 23:56:22 -0400

    Added support for episodic output from daily inventories in temporal allocation module

--- EMF/deploy/db/tempalloc/run_temporal_allocation_daily.sql
+++ EMF/deploy/db/tempalloc/run_temporal_allocation_daily.sql
@@ -26,8 +26,11 @@ DECLARE
   
   days_in_month smallint;
   column_sql text := '';
+  
+  episode_sql text := '';
   loop_date date;
   is_weekend boolean;
+  add_day boolean;
 BEGIN
 
   -- get the inventory table name
@@ -98,7 +101,10 @@ BEGIN
     FROM emf.internal_sources i
    WHERE i.dataset_id = episodic_result_dataset_id;
 
-  IF resolution LIKE '%month%' THEN
+  IF resolution LIKE '%daily total%' THEN
+    RETURN;
+
+  ELSIF resolution LIKE '%month%' THEN
 
     FOR month_num IN EXTRACT(MONTH FROM start_day)..EXTRACT(MONTH FROM end_day) LOOP
 
@@ -110,8 +116,8 @@ BEGIN
         column_sql := column_sql || ' + inv.dayval' || i;
       END LOOP;
   
---      EXECUTE '
-      RAISE NOTICE '%', '
+--      RAISE NOTICE '%', '
+      EXECUTE '
       INSERT INTO emissions.' || monthly_result_table_name || ' (
              dataset_id,
              poll,
@@ -150,7 +156,107 @@ BEGIN
          SET avg_day_emis = total_emis / days_in_month';
        
     END LOOP;
+  
+  ELSE
+  
+    loop_date := start_day;
+    FOR month_num IN EXTRACT(MONTH FROM start_day)..EXTRACT(MONTH FROM end_day) LOOP
+    
+      -- build list of columns to sum for episode dates in current month
+      days_in_month := 0;
+      column_sql := '';
+      LOOP
+        is_weekend := EXTRACT(DOW FROM loop_date) = 0 OR EXTRACT(DOW FROM loop_date) = 6;
+
+        IF resolution LIKE '%weekday%' THEN
+          add_day := NOT is_weekend;
+        ELSIF resolution LIKE '%weekend%' THEN
+          add_day := is_weekend;
+        ELSE
+          add_day := true;
+        END IF;
+        
+        IF add_day THEN
+          days_in_month := days_in_month + 1;
+          
+          IF LENGTH(column_sql) > 0 THEN
+            column_sql := column_sql || ' + ';
+          END IF;
+          
+          column_sql := column_sql || 'inv.dayval' || to_char(loop_date, 'FMDD');
+        END IF;
       
+        loop_date := loop_date + 1;
+        EXIT WHEN loop_date > end_day OR EXTRACT(MONTH FROM loop_date) != month_num;
+      END LOOP;
+      
+      -- skip month if no days to process
+      IF LENGTH(column_sql) = 0 THEN
+        CONTINUE;
+      END IF;
+    
+      IF LENGTH(episode_sql) > 0 THEN
+        episode_sql := episode_sql || ' UNION ';
+      END IF;
+    
+      episode_sql := episode_sql || '
+        SELECT inv.poll,
+               inv.scc,
+               inv.region_cd AS fips,
+               ' || inv_plantid || ' AS plantid,
+               ' || inv_pointid || ' AS pointid,
+               ' || inv_stackid || ' AS stackid,
+               ' || inv_processid || ' AS processid,
+               ' || column_sql || ' AS emis,
+               ' || days_in_month || ' AS days,
+               inv.dataset_id
+          FROM emissions.' || inventory_table_name || ' inv
+         WHERE ' || inv_filter || '
+           AND inv.monthnum = ' || month_num;
+               
+    END LOOP;
+
+--    RAISE NOTICE '%', '
+    EXECUTE '
+    INSERT INTO emissions.' || episodic_result_table_name || '(
+           dataset_id,
+           poll,
+           scc,
+           fips,
+           plantid,
+           pointid,
+           stackid,
+           processid,
+           total_emis,
+           days_in_episode,
+           inv_dataset_id
+    )
+    SELECT ' || episodic_result_dataset_id || ',
+           v.poll,
+           v.scc,
+           v.fips,
+           v.plantid,
+           v.pointid,
+           v.stackid,
+           v.processid,
+           SUM(emis),
+           SUM(days),
+           v.dataset_id
+      FROM (' || episode_sql || ') v
+  GROUP BY v.dataset_id,
+           v.poll,
+           v.scc,
+           v.fips,
+           v.plantid,
+           v.pointid,
+           v.stackid,
+           v.processid';
+  
+    -- calculate episodic average day emissions
+    EXECUTE '
+    UPDATE emissions.' || episodic_result_table_name || '
+       SET avg_day_emis = total_emis / days_in_episode';
+  
   END IF;
   
 END;
