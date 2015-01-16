bd9f40aee87ddf075e7485e6b7df44603791641b
Catherine Seppanen <cas@whpress.com>
2015-01-16 15:45:01 -0500

    Updated version for 2.7.4

--- EMF/src/client/gov/epa/emissions/framework/client/login/LoginWindow.java
+++ EMF/src/client/gov/epa/emissions/framework/client/login/LoginWindow.java
@@ -50,7 +50,7 @@ public class LoginWindow extends EmfFrame implements LoginView {
 
     private ServiceLocator serviceLocator;
 
-    public final static String EMF_VERSION = "v2.7.3 - 12/18/2014";
+    public final static String EMF_VERSION = "v2.7.4 - 01/16/2015";
 
     public LoginWindow(ServiceLocator serviceLocator) {
         super("Login", "Login to the Emissions Modeling Framework [" + EMF_VERSION + "]");
