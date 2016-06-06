452eb4f2624f218370d0035cad8724337c0c2bca
Catherine Seppanen <cseppan@email.unc.edu>
2016-06-06 16:45:44 -0400

    Use Float object for SCC combustion efficiency so it can be set to null

--- EMF/src/contract/gov/epa/emissions/framework/services/cost/controlmeasure/Scc.java
+++ EMF/src/contract/gov/epa/emissions/framework/services/cost/controlmeasure/Scc.java
@@ -13,7 +13,7 @@ public class Scc {
     private String status;
     private String sector,ei_category,scc_l1,scc_l2,scc_l3,scc_l4,last_inventory_year,map_to,created_date,revised_date,option_group,option_set,short_name;
     
-    private float combustionEfficiency;
+    private Float combustionEfficiency;
     		
     public Scc() {
         // Empty
