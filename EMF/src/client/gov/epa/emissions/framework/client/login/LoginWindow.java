9e7fb650645337278f4eb1f7cc8a65af7e8e78f9
Paul Andrei <pandrei@foliage.com>
2017-07-18 01:39:35 -0400

    Bumped build version to "Upstream Beta 13 Build #102 - 7/18/2017".

--- EMF/src/client/gov/epa/emissions/framework/client/login/LoginWindow.java
+++ EMF/src/client/gov/epa/emissions/framework/client/login/LoginWindow.java
@@ -48,7 +48,7 @@ public class LoginWindow extends EmfFrame implements LoginView {
     private ServiceLocator serviceLocator;
 
 //  public final static String EMF_VERSION = "v2.14 - 06/27/2017";
-    public final static String EMF_VERSION = "Upstream Beta 12 Build #94 - 6/26/2017";
+    public final static String EMF_VERSION = "Upstream Beta 13 Build #102 - 7/18/2017";
 
     public LoginWindow(ServiceLocator serviceLocator) {
         super("Login", "Login to EMF [" + EMF_VERSION + "]");
