71b92563ecfc6d6f6666d01e75312a083f72c32c
Catherine Seppanen <cas@whpress.com>
2018-07-06 01:53:17 -0400

    Remove old MultiMarkdown processing scripts and files

--- EMF/deploy/doc/guides/cost/gen_cost_guide.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cat ch1_introduction.md ch2_installing_cost.md ch3_control_measure_manager.md ch4_control_strategy_manager.md ch5_control_strategy_exercises.md ch6_example_sql.md ch7_references.md acronyms.md > rendered/all_chapters.md
-./parse_all_txt.pl rendered/all_chapters.md > rendered/cost_guide.md
-cd rendered
-multimarkdown cost_guide.md
-cd ..
--- EMF/deploy/doc/guides/gen_temp_alloc.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-parse_proj.pl temporal_allocation.txt > rendered/temporal_allocation.txt; cd rendered; ~/Library/Application\ Support/MultiMarkdown/bin/mmd-xslt temporal_allocation.txt; cd ..
