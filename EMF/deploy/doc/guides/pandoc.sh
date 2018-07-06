ab96cadc2341b834dd9c539d2491758292296ca3
Catherine Seppanen <cas@whpress.com>
2018-07-05 22:45:20 -0400

    Add user guide generation script

---
+++ EMF/deploy/doc/guides/pandoc.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+BASEDIR=.
+VERSION="3.1"
+
+$BASEDIR/pandoc-2.2.1/bin/pandoc --standalone --filter $BASEDIR/pandoc-crossref/pandoc-crossref -M chapters=true -M numberSections=true -M sectionsDepth=-1 -M linkReferences=true -M nameInLink=true --metadata title="Emissions Modeling Framework v$VERSION User's Guide" --metadata subtitle="Last updated: `date "+%B %e, %Y"`" --toc --toc-depth=2 --css base.css -o ./output/index.html ch1_introduction.md ch2_client.md ch3_datasets.md ch4_dataset_qa.md temporal_allocation.md troubleshooting.md server_admin.md
+
