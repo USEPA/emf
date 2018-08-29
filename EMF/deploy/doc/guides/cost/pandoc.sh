f6137f8e4d4a94d1496d6138c56d8849c0224391
Catherine Seppanen <cas@whpress.com>
2018-08-29 03:22:50 -0400

    Update documentation version to 3.2

--- EMF/deploy/doc/guides/cost/pandoc.sh
+++ EMF/deploy/doc/guides/cost/pandoc.sh
@@ -3,7 +3,7 @@
 # activate virtualenv before running
 
 BASEDIR=.
-VERSION="3.1"
+VERSION="3.2"
 
 $BASEDIR/pandoc-2.2.1/bin/pandoc --standalone --filter pandoc-fignos --filter pandoc-tablenos --filter ./filter/comments.py --number-sections -M xnos-number-sections=On --metadata title="CoST v$VERSION User's Guide" --metadata subtitle="Last updated: `date "+%B %e, %Y"`" --toc --toc-depth=3 --css base.css -o ./output/index.html ch1_introduction.md ch2_installing_cost.md ch3_control_measure_manager.md ch4_control_strategy_manager.md ch5_control_strategy_exercises.md ch6_example_sql.md ch7_references.md acronyms.md
 
