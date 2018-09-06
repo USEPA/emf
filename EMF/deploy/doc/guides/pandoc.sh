a6a6c834d30f922ed1b411118d96165fe3174dd6
Catherine Seppanen <cas@whpress.com>
2018-09-06 03:39:07 -0400

    Add chapter on module types and modules to EMF User's Guide

--- EMF/deploy/doc/guides/pandoc.sh
+++ EMF/deploy/doc/guides/pandoc.sh
@@ -3,5 +3,5 @@
 BASEDIR=.
 VERSION="3.2"
 
-$BASEDIR/pandoc-2.2.1/bin/pandoc --standalone --filter $BASEDIR/pandoc-crossref/pandoc-crossref -M chapters=true -M numberSections=true -M sectionsDepth=-1 -M linkReferences=true -M nameInLink=true --metadata title="Emissions Modeling Framework v$VERSION User's Guide" --metadata subtitle="Last updated: `date "+%B %e, %Y"`" --toc --toc-depth=2 --css base.css -o ./output/index.html ch1_introduction.md ch2_client.md ch3_datasets.md ch4_dataset_qa.md ch5_case_management.md temporal_allocation.md inventory_projection.md troubleshooting.md server_admin.md
+$BASEDIR/pandoc-2.2.1/bin/pandoc --standalone --filter $BASEDIR/pandoc-crossref/pandoc-crossref -M chapters=true -M numberSections=true -M sectionsDepth=-1 -M linkReferences=true -M nameInLink=true --metadata title="Emissions Modeling Framework v$VERSION User's Guide" --metadata subtitle="Last updated: `date "+%B %e, %Y"`" --toc --toc-depth=2 --css base.css -o ./output/index.html ch1_introduction.md ch2_client.md ch3_datasets.md ch4_dataset_qa.md ch5_case_management.md temporal_allocation.md inventory_projection.md module_types_and_modules.md troubleshooting.md server_admin.md
 
