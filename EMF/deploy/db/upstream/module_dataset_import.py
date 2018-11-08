d9a0bd100d55955bec3d89a3ed51e674999891ab
Catherine Seppanen <cseppan@email.unc.edu>
2018-11-08 11:10:21 -0500

    Add command line script to import datasets used by a module

---
+++ EMF/deploy/db/upstream/module_dataset_import.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+import argparse
+import os
+import xml.etree.ElementTree as ET
+
+parser = argparse.ArgumentParser(description='Import datasets associated with a module.')
+parser.add_argument('-x', '--xml', help='module export XML file', required=True)
+parser.add_argument('-d', '--data', help='server directory where the exported datasets are located', required=True)
+parser.add_argument('-c', '--client', help='local directory where the EMF client is installed', required=True)
+parser.add_argument('-t', '--tomcat', help='Tomcat server address with port (if needed)', default='http://localhost:8080')
+parser.add_argument('-k', '--jobkey', help='import job key code', required=True)
+args = parser.parse_args()
+
+classpath = ''
+for file in os.listdir(os.path.join(args.client, 'lib')):
+  if file.endswith('.jar'):
+    classpath += os.path.join(args.client, file) + ':'
+classpath += os.path.join(args.client, 'emf-client.jar')
+
+tree = ET.parse(args.xml)
+root = tree.getroot()
+
+for dataset in root.findall(".//*[@property='moduleDatasetInfo']//*[@class='java.util.HashMap']"):
+  datasetInfo = {}
+  for entry in dataset:
+    datasetInfo[entry[0].text] = entry[1].text
+  
+  print('Importing dataset ' + datasetInfo['datasetName'])
+  cmd = ['java', '-classpath', classpath,
+         'gov.epa.emissions.framework.client.EMFCmdClient', args.tomcat + '/emf/services', '-d',
+         '-k', args.jobkey, '-F', os.path.join(args.data, datasetInfo['exportName']),
+         '-T', "'" + datasetInfo['datasetTypeName'] + "'",
+         '-N', "'" + datasetInfo['datasetName'] + "'"]
+  os.system(' '.join(cmd))
