169932ce762516933586e630e7e90e3c21433691
Catherine Seppanen <cseppan@email.unc.edu>
2020-08-17 20:27:04 -0400

    Version 3.7

--- EMF/src/client/gov/epa/emissions/framework/client/login/LoginWindow.java
+++ EMF/src/client/gov/epa/emissions/framework/client/login/LoginWindow.java
@@ -47,7 +47,7 @@ public class LoginWindow extends EmfFrame implements LoginView {
 
     private ServiceLocator serviceLocator;
 
-    public final static String EMF_VERSION = "v3.6 - 05/15/2020";
+    public final static String EMF_VERSION = "v3.7 - 08/17/2020";
 
     public LoginWindow(ServiceLocator serviceLocator) {
         super("Login", "Login to EMF [" + EMF_VERSION + "]");
