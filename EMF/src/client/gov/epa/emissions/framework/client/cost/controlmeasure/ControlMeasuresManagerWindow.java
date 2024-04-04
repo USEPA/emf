728ab13fff35cf0c660639ed5e0fa6060df5387a
Catherine Seppanen <cseppan@email.unc.edu>
2024-04-04 02:20:24 -0400

    Update the Cost Year menu to include 2019 through 2022 #147

--- EMF/src/client/gov/epa/emissions/framework/client/cost/controlmeasure/ControlMeasuresManagerWindow.java
+++ EMF/src/client/gov/epa/emissions/framework/client/cost/controlmeasure/ControlMeasuresManagerWindow.java
@@ -89,7 +89,7 @@ public class ControlMeasuresManagerWindow extends DisposableInteralFrame impleme
 
     private String[] years = { "2000", "2001", "2002", "2003", "2004", "2005", "2006",
             "2007", "2008", "2009", "2010", "2011", "2012", "2013", "2014", "2015", "2016", "2017",
-            "2018" };
+            "2018", "2019", "2020", "2021", "2022" };
 
     private CostYearTable costYearTable;
 
